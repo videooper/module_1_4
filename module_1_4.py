4ca045cc7e5a97661140fea83a7b6d60e2891084
videooper <97392291+videooper@users.noreply.github.com>
2024-11-12 21:40:19 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,22 @@
+# Создайте переменную my_string и присвойте ей значение строки с произвольным текстом (функция input()).
+my_string = "*****Здравствуйте, уважаемый преподаватель!*****"
+print("Входная строка:", my_string)
+
+# Вывести количество символов введённого текста
+character_count = len(my_string)
+print("Количество символов:", character_count)
+
+# Выведите строку my_string в верхнем регистре.
+print(my_string .upper())
+
+# Выведите строку my_string в нижнем регистре.
+print(my_string .lower())
+
+# Измените строку my_string, удалив все пробелы.
+print(my_string .replace(" ", ""))
+
+# Выведите первый символ строки my_string.
+print(my_string[0])
+
+# Выведите последний символ строки my_string.
+print(my_string[-1])
